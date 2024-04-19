473582346694e363c7b3289bcc6dae4a48c2cdb5
Kien <43236511+kien-pham@users.noreply.github.com>
2024-04-19 13:07:55 +0700

    feat: add SplashScreen

--- src/screens/splash-screen/splash-screen.tsx
+++ src/screens/splash-screen/splash-screen.tsx
@@ -1,19 +1,40 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
-import LinearGradient from "react-native-linear-gradient";
+import { LinearGradient } from "expo-linear-gradient";
 import { IMAGES } from "src/constant/images";
 import { THEME } from "src/constant/theme";
 import { styles } from "./style";
+import ActionButton from "src/components/button/button";
 
 export default function SplashScreen() {
   return (
     <View style={styles.container}>
       <Image source={IMAGES.SplashScreenBg} resizeMode="contain" />
+
       <LinearGradient
         style={styles.gradientBg}
         colors={[THEME.colors.transparent, THEME.colors.black]}
       >
-        <Text>test</Text>
+        <View style={styles.contentContainer}>
+          <Text
+            style={styles.mainTitle}
+          >{`Cooking a \nDelicious Food \nEasily`}</Text>
+          <Text style={styles.subTitle}>
+            Discover more than 1200 food recipes in your hands and cooking it
+            easily!
+          </Text>
+
+          <ActionButton
+            label="Login"
+            btnStyle={styles.loginBtn}
+            textStyle={styles.loginBtnLabel}
+          />
+          <ActionButton
+            label="Sign up"
+            btnStyle={styles.signupBtn}
+            textStyle={styles.loginBtnLabel}
+          />
+        </View>
       </LinearGradient>
     </View>
   );
--- src/screens/splash-screen/style.ts
+++ src/screens/splash-screen/style.ts
@@ -1,4 +1,5 @@
-import { StyleSheet } from "react-native";
+import { DimensionValue, StyleSheet } from "react-native";
+import { THEME } from "src/constant/theme";
 
 export const styles = StyleSheet.create({
   container: {
@@ -6,7 +7,49 @@ export const styles = StyleSheet.create({
   },
   gradientBg: {
     position: "absolute",
+    width: THEME.sizes.full as DimensionValue,
+    height: THEME.sizes.full as DimensionValue,
+  },
+  contentContainer: {
+    position: "absolute",
+    bottom: 16,
     width: "100%",
-    height: "100%",
+    padding: THEME.spacing["2xl"],
+  },
+  mainTitle: {
+    color: THEME.colors.white,
+    fontSize: THEME.fontSizes["4xl"],
+    fontWeight: "bold",
+    marginBottom: THEME.spacing.lg,
+  },
+  subTitle: {
+    color: THEME.colors.gray[50],
+    fontSize: THEME.fontSizes.sm,
+    fontWeight: "400",
+    marginBottom: THEME.spacing["2xl"],
+  },
+  loginBtn: {
+    padding: THEME.spacing.xl,
+    marginBottom: THEME.spacing.xl,
+    backgroundColor: THEME.colors.cygan[400],
+    borderRadius: THEME.spacing.md,
+  },
+  loginBtnLabel: {
+    color: THEME.colors.white,
+    fontSize: THEME.fontSizes.sm,
+    fontWeight: "700",
+    textTransform: "uppercase",
+  },
+  signupBtn: {
+    padding: THEME.spacing.xl,
+    borderRadius: THEME.spacing.md,
+    borderWidth: 1,
+    borderColor: THEME.colors.white,
+  },
+  signupBtnLabel: {
+    color: THEME.colors.white,
+    fontSize: THEME.fontSizes.sm,
+    fontWeight: "bold",
+    textTransform: "uppercase",
   },
 });
